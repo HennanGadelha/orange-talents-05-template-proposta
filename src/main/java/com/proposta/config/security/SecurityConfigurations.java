b1e0f5d20810330b9667c7705f2053b9b89f87e7
Hennan Gadelha <hennanhennan@gmail.com>
2021-06-16 15:48:58 -0300

    endpoint prometheus

--- src/main/java/com/proposta/config/security/SecurityConfigurations.java
+++ src/main/java/com/proposta/config/security/SecurityConfigurations.java
@@ -1,6 +1,7 @@
 package com.proposta.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
@@ -11,7 +12,8 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
+        http.authorizeRequests(authorizeRequests -> authorizeRequests.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                .anyRequest().authenticated())
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
